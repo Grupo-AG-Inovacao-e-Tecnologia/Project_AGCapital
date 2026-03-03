27cd106d1c4cd894e28d502494107a7f6a4bc3f3
France Dev <france.dev@outlook.com>
2026-03-03 01:51:03 -0300

    feat: create sidebar-items resource with user information and navigation structure for the application

---
+++ src/resources/sidebar-items.ts
@@ -0,0 +1,53 @@
+import { paths } from "@/lib/paths";
+import {
+  IconDashboard,
+  IconMessage,
+  IconReport,
+  IconUsers,
+  IconUserPlus,
+} from "@tabler/icons-react";
+
+export type SidebarSection = {
+  name: string;
+  items: {
+    name: string;
+    url: string;
+    icon: React.ElementType;
+  }[];
+};
+
+export const sidebarItems = {
+  user: {
+    name: "FranceTech",
+    email: "francetech@gmail.com",
+    avatar: "/logo.png",
+  },
+  navMain: [
+    {
+      name: "Dashboard",
+      url: paths.home,
+      icon: IconDashboard,
+    },
+    {
+      name: "Enrollment Leads",
+      url: paths.enrollmentLeads,
+      icon: IconUserPlus,
+    },
+    {
+      name: "Relatórios",
+      url: paths.reports,
+      icon: IconReport,
+    },
+    {
+      name: "Chat",
+      url: paths.chat,
+      icon: IconMessage,
+    },
+    {
+      name: "Funcionários",
+      url: paths.employees,
+      icon: IconUsers,
+    },
+  ],
+  sections: [] as SidebarSection[],
+};
