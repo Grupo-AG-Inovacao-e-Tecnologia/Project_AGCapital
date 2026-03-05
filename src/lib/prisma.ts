10da060f3b63b8ef332536d0c014ba900354be01
France Dev <france.dev@outlook.com>
2026-03-05 00:39:12 -0300

    chore: update database adapter from SQLite to PostgreSQL and refactor connection handling

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,10 @@
 import { PrismaClient } from "../../prisma/generated/prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL,
-});
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export { prisma };
