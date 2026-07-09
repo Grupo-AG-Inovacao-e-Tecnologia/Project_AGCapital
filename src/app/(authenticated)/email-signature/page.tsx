b52f7fe5868631c32f3754d6f5c2dfd75ae44f4e
Ariel Franca <france.dev@outlook.com>
2026-07-09 13:57:02 -0300

    Trigger redeploy

--- src/app/(authenticated)/email-signature/page.tsx
+++ src/app/(authenticated)/email-signature/page.tsx
@@ -7,7 +7,7 @@ import { EmailSignatureGenerator } from "./email-signature-generator";
 export default function Page() {
   return (
     <>
-      <SiteHeader title="Assinatura de Email" />
+      <SiteHeader title="Assinatura de Email " />
       <div className="flex flex-1 flex-col">
         <div className="@container/main flex flex-1 flex-col gap-2">
           <div className="flex flex-col gap-4 p-4 md:gap-6 md:y-6">
